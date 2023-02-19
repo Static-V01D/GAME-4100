9c70c4d307b0fb9b2133d1521ed97afdd13a1406
Static-V01D <101192162+Static-V01D@users.noreply.github.com>
2023-02-18 20:16:41 -0400

    Character controller
    I implemented the character controller and moved the camera to an isometric position.

--- My project/Assets/LACG_Scripts/Character_Controller.cs
+++ My project/Assets/LACG_Scripts/Character_Controller.cs
@@ -2,9 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character_Controller : MonoBehaviour
+public class Character_Controller : MonoBehaviour 
 {
-  
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float Speed = 5;
+    [SerializeField] private float TurnSpeed = 360;
+    private Vector3 _input;   
+
+   
+    private void Update()
+    {
+       
+        GatherInput();
+        Look();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    } 
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Look()
+    {
+        if (_input == Vector3.zero) return;
+
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, TurnSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward * _input.normalized.magnitude * Speed * Time.deltaTime);
+    }   
+   
 }
 
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
+}
 
