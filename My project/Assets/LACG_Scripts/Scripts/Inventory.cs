b5f26e50c5925256354c89ff8aff6097455a8bce
Static-V01D <101192162+Static-V01D@users.noreply.github.com>
2023-03-19 00:56:46 -0400

    Inventory

--- My project/Assets/LACG_Scripts/Scripts/Inventory.cs
+++ My project/Assets/LACG_Scripts/Scripts/Inventory.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Inventory 
 {
+    public event EventHandler OnItemListChanged;
+
     private List<Item> itemList;
 
     public Inventory()
@@ -11,12 +14,43 @@ public class Inventory
         itemList = new List<Item> ();
 
         AddItem(new Item{ itemType = Item.ItemType.Sword, amount = 1 });
+        AddItem(new Item { itemType = Item.ItemType.HealthPotion, amount = 1 });
+        AddItem(new Item { itemType = Item.ItemType.Manapotion, amount = 1 });
+
 
         Debug.Log(itemList.Count);
     }
 
     public void AddItem(Item item)
     {
-        itemList.Add (item);
+        if (item.IsStackable())
+        {
+            bool itemAlreadyInInventory = false;
+            foreach(Item inventoryItem in itemList)
+            {
+                if(inventoryItem.itemType == item.itemType)
+                {
+                    inventoryItem.amount += item.amount;
+                    itemAlreadyInInventory = true;
+                }
+            }
+            if(!itemAlreadyInInventory) 
+            {
+
+                itemList.Add(item);
+            }
+        }
+        else
+        {
+            itemList.Add(item);
+        }
+        OnItemListChanged?.Invoke(this, EventArgs.Empty);
+
+       
+    }
+
+    public List<Item> GetItemList()
+    {
+        return itemList;
     }
 }
--- My project/Assets/LACG_Scripts/Scripts/Item.cs
+++ My project/Assets/LACG_Scripts/Scripts/Item.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class Item 
 { 
     public enum ItemType
@@ -17,5 +19,46 @@ public class Item
     public ItemType itemType;
     public int amount;
 
+    public Sprite GetSprite()
+    {
+        switch (itemType) 
+        {
+            default:
+            case ItemType.Sword: return ItemAssets.Instance.swordSprite;
+            case ItemType.HealthPotion: return ItemAssets.Instance.healthPotionSprite;
+            case ItemType.Manapotion: return ItemAssets.Instance.manaPotionSprite;
+            case ItemType.Coin: return ItemAssets.Instance.coinSprite;
+            case ItemType.Medkit: return ItemAssets.Instance.medKitSprite;
+        }
+    }
 
+    public bool IsStackable()
+    {
+        switch(itemType) 
+        {
+            default:
+            case ItemType.Coin:
+            case ItemType.Manapotion:
+            case ItemType.HealthPotion:
+                return true;      
+               
+            case ItemType.Medkit:
+            case ItemType.Sword:
+                return false;
+        }
+    }
+  /*  public Color GetColor() 
+    {
+        switch(itemType) 
+        {
+            
+            default:
+            case ItemType.Sword: return new Color(0, 0, 0);
+            case ItemType.HealthPotion: return new Color(1, 0, 0);
+            case ItemType.Manapotion: return new Color(0, 0, 1);
+            case ItemType.Coin: return new Color(1, 1, 0);
+            case ItemType.Medkit: return new Color(0, 1, 1);
+
+        }
+    }*/
 }
