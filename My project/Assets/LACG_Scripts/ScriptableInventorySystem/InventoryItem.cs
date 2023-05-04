e96fd7a609e352bbc8f20f51cbbf79fd15f7d252
Static-V01D <101192162+Static-V01D@users.noreply.github.com>
2023-05-04 19:00:54 -0400

    Scriptable items

---
+++ My project/Assets/LACG_Scripts/ScriptableInventorySystem/InventoryItem.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class InventoryItem 
+{
+   public ItemData itemData;
+    public int StackSize;
+
+
+    public InventoryItem(ItemData item)
+    {
+        itemData = item;
+        AddToStack();
+    }
+
+    public void AddToStack()
+    {
+
+        StackSize++;
+    }
+
+    public void RemoveFromStack() 
+    {
+        
+        StackSize--;
+    
+    }
+}
---
+++ My project/Assets/LACG_Scripts/ScriptableInventorySystem/ScriptableInventory.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableInventory : MonoBehaviour
+{
+    public List<InventoryItem> inventory = new List<InventoryItem>();
+
+    private Dictionary<ItemData, InventoryItem> itemDictionary = new Dictionary<ItemData, InventoryItem>();
+
+
+    private void OnEnable()
+    {
+        CubeCollect.Collected += Add;
+    }
+    private void OnDisable ()
+    {
+          CubeCollect.Collected -= Add;
+    }
+    public void Add(ItemData itemData)
+    {
+        if(itemDictionary.TryGetValue(itemData, out InventoryItem item)) 
+        {
+            item.AddToStack();
+            Debug.Log($" {item.itemData.displayName} total stack is now {item.StackSize}");
+        }
+        else
+        {
+
+            InventoryItem newItem = new InventoryItem(itemData);
+            inventory.Add(newItem);
+            itemDictionary.Add(itemData, newItem);
+            Debug.Log($" Added {itemData.displayName} to the inventory for the first time");
+        }
+    }
+
+
+    public void Remove(ItemData itemData) 
+    {
+
+
+        if (itemDictionary.TryGetValue(itemData, out InventoryItem item))
+        {
+            item.RemoveFromStack();
+            if(item.StackSize == 0)
+            {
+                inventory.Remove(item);
+                itemDictionary.Remove(itemData);
+            }
+
+        }
+        
+
+    }
+}
