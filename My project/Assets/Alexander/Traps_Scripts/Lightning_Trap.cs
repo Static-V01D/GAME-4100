6aad72beb2eb9c11562e37287cf83c3b8175362b
Alexander Rodriguez <100698551+Alexios12345@users.noreply.github.com>
2023-05-07 19:04:17 -0400

    Added assets from asset list

--- My project/Assets/Alexander/Traps_Scripts/Lightning_Trap.cs
+++ My project/Assets/Alexander/Traps_Scripts/Lightning_Trap.cs
@@ -1,30 +1,32 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Security.Cryptography.X509Certificates;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
 
-//public class Lightning_Trap : MonoBehaviour
-//{
-//    //Variables
-//    MeshCollider electric;
-//        public int slow = 5;
-//        public int Damage = 100;
-//        public int Trap_Health = 100;
+public class Lightning_Trap : MonoBehaviour
+{
+    //Variables
+    Transform Enemy;
+    GameObject Water_puddle;
+    public int slow = 5;
+    public int Damage = 100;
+    public int Trap_Health = 100;
 
-//    void Start()
-//    {
-//        electric = this.GetComponent<MeshCollider>();
-//    }
+    void Start()
+    {
+        Enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
+    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
         
-//    }
 
-//    void OnTriggerEnter(Collider other)
-//    {
-//        //Destroy(other.gameObject);
-
-//    }
-//}
+    }
+}
